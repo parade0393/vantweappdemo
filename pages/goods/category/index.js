285a2f1a065a5f0febdf068ec49ea070a008f2f5
flb <13703937803@163.com>
2025-05-30 09:12:01 +0800

    提交沉浸式案例

--- pages/goods/category/index.js
+++ pages/goods/category/index.js
@@ -1,73 +1,31 @@
-// pages/goods/category/index.js
+// pages/index/index.js
+const themeUtil = require('../../../utils/theme');
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    count:1
+    theme: 'light',
+    themeClass: '',
+    statusBarHeight: 0,
+    navHeight: 0,
+    menuRight: 0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-   
-  },
-
-  increaceCount(){
-    console.log("increace");
+  onLoad: function (options) {
+    // 初始化主题
+    themeUtil.watchThemeChange(this);
+    const {top,height,right,width,left } = wx.getMenuButtonBoundingClientRect()
+    const {statusBarHeight,windowWidth} = wx.getWindowInfo()
+    console.log(statusBarHeight,top)
+    console.log(windowWidth,left,right,width)
+    const navHeight = statusBarHeight + (top-statusBarHeight)*2+height
     this.setData({
-      count:++this.data.count
+      statusBarHeight,
+      menuTop:top+(height/4),
+      navHeight,
+      menuRight: windowWidth-left+10
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-    this.getTabBar().init();
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  },
-})
+  onShow: function () {
+  }
+});
